d2fd5f419fa342aec7a1ff78771dda499b577164
Thinh Duc <ndt7.vn@gmail.com>
2022-02-21 15:48:31 +0700

    giao diện học sinh

---
+++ QL_hs_gv/QL_hs_gv/GUI/frmHocSinh.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QL_hs_gv.GUI
+{
+    public partial class frmHocSinh : Form
+    {
+        public frmHocSinh()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Thêm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
